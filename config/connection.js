4d6e57fd0beaef92388dbca775d2b9775e22d451
DarrenSmith10 <darrensmith@daksmith.co.uk>
2025-03-08 09:36:05 +0000

    Updated MySQL connection for Clever Cloud

--- config/connection.js
+++ config/connection.js
@@ -14,9 +14,15 @@ const sequelize = process.env.JAWSDB_URL
       process.env.DB_USERNAME,
       process.env.DB_PASSWORD,
       {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT,
+        // host: process.env.DB_HOST,
+        // dialect: process.env.DB_DIALECT,
+        // port: process.env.DB_PORT,
+        dialectOptions: {
+          ssl: {
+            require: true, // Enable SSL
+            rejectUnauthorized: false,
+          },
+        },
       }
     );
 
